672bccb1665568e326b09e0cdb5723ce6919faea
Chhhhong <Cheonhong450@gmail.com>
2022-06-27 21:54:21 +0900

    C# DynamicArray
    C# DynamicArray

--- Example03_DynamicArray/Program.cs
+++ Example03_DynamicArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Example03_DynamicArray
 {
@@ -6,7 +7,22 @@ namespace Example03_DynamicArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DynamicArray dynamicArray = new DynamicArray();
+            dynamicArray.Add(5);
+            dynamicArray.Add(4);
+            dynamicArray.Add(3);
+            dynamicArray.Add(2);
+            dynamicArray.Add(1);
+            Console.WriteLine(dynamicArray.Length);
+            Console.WriteLine(dynamicArray.Capacity);
+            Console.WriteLine(dynamicArray[3]);
+            dynamicArray[3] = 10;
+            Console.WriteLine(dynamicArray[3]);
+
+            List<int> list = new List<int>();
+            list.Add(5);
+            list.Remove(5);
+            list.RemoveAt(0);
         }
     }
 }
