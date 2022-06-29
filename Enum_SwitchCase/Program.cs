4c175a21472c8a4c7649bb1a81ae32ad3d3f48be
Chhhhong <Cheonhong450@gmail.com>
2022-06-29 21:53:08 +0900

    C# enum
    C# enum

---
+++ Enum_SwitchCase/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+// enum (enumerated type) 열거형
+// enum 의 기본요소는 모두 int 형
+enum PlayerState
+{
+    Idle,
+    Attack,
+    Jump,
+    Walk,
+    Run,
+    Dash,
+    Home,
+}
+
+namespace Enum_SwitchCase
+{
+    internal class Program
+    {
+        static bool doAttack = true;
+        static bool doJump;
+        static bool doWalk;
+        static bool doRun;
+        static bool doDash;
+        static bool doHome;
+
+        static PlayerState initState = PlayerState.Attack;
+
+        static void Main(string[] args)
+        {
+            Warrior warrior = new Warrior();
+            warrior.name = "소드마스터";
+            if (doAttack)
+                warrior.Attack();
+            else if (doJump)
+                warrior.Jump();
+            else if (doWalk)
+                warrior.Walk();
+            else if (doRun)
+                warrior.Run();
+            else if (doDash)
+                warrior.Dash();
+            else if (doHome)
+                warrior.Home();
+            else
+                Console.WriteLine("전사 가만히 있음");
+
+            switch (initState)
+            {
+                case PlayerState.Idle:
+                    break; // 현재 구문을 빠져나오는 분기문
+                case PlayerState.Attack:
+                    warrior.Attack();
+                    break;
+                case PlayerState.Jump:
+                    warrior.Jump();
+                    break;
+                case PlayerState.Walk:
+                    warrior.Walk();
+                    break;
+                case PlayerState.Run:
+                    warrior.Run();
+                    break;
+                case PlayerState.Dash:
+                    warrior.Dash();
+                    break;
+                case PlayerState.Home:
+                    warrior.Home();
+                    break;
+                default:
+                    break;
+            }
+
+
+            string studentName = "";
+            switch (studentName)
+            {
+                case "철수":
+                    break;
+                case "영희":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    class Warrior
+    {
+        public string name;
+        public void Attack()
+        {
+            Console.WriteLine($"{name} (이)가 공격함");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine($"{name} (이)가 점프함");
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine($"{name} (이)가 걸음");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"{name} (이)가 달림");
+        }
+
+        public void Dash()
+        {
+            Console.WriteLine($"{name} (이)가 돌진함");
+        }
+
+        public void Home()
+        {
+            Console.WriteLine($"{name} (이)가 귀환함");
+        }
+    }
+
+
+
+}
--- GenericCollections/Program.cs
+++ GenericCollections/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 
+
 // Generic : 일반화
 // 다양한 자료형에 대해서 유동적으로 갖다쓸 수 있도록 만드는 형태
 namespace GenericCollections
@@ -10,6 +11,20 @@ namespace GenericCollections
     {
         static void Main(string[] args)
         {
+            // 박싱 : 자료형을 객체타입으로 변환하는 과정
+            // 언박싱 : 객체타입을 원래 자료형을 변환하는 과정
+            // System.Collections 를 더이상 사용하지 않는 이유는
+            // 박싱은 기본형 단순 할당과정보다 20배 정도 느리고
+            // 언박싱은 4배 정도 느리기때문에
+            // Generic 을 사용할것을 권장하고있다.
+
+            // ArrayList (박싱 / 언박싱 때문에 쓰지않음)
+            //--------------------------------------------------------
+            System.Collections.ArrayList arrayList = new System.Collections.ArrayList();
+            arrayList.Add(3);
+            arrayList.Add("철수");
+            arrayList.Add('A');
+
             // List
             List<int> list_int = new List<int>();
             List<float> list_float = new List<float>();
@@ -29,14 +44,26 @@ namespace GenericCollections
             //list_int.Find(x => x == 0);
             list_int.Contains(0);
 
+            for (int i = 0; i < list_int.Count; i++)
+            {
+                Console.WriteLine(list_int[i]);
+            }
+
 
             // LinkedList
             LinkedList<int> linkedList = new LinkedList<int>();
+
+            // 추가
             linkedList.AddLast(0);
             linkedList.AddFirst(1);
             linkedList.AddBefore(linkedList.Find(0) , 3);
             Console.WriteLine(linkedList.First);
+
+            // 삭제
             linkedList.RemoveLast();
+
+            // 검색
+            linkedList.Find(0);
 
             // 중요!
             // *****문자열 키값으로 해시를 뽑아내는 해시함수 구현 방식*****
@@ -50,11 +77,72 @@ namespace GenericCollections
             // 
             // 2. 오픈어드레싱
 
+            // ArrayList (박싱 / 언박싱 때문에 쓰지않음)
+            //--------------------------------------------------------
+            System.Collections.Hashtable ht = new System.Collections.Hashtable();
+            ht.Add("철수", 90);
+
             // Dictionary
+            Dictionary<string, char> grades = new Dictionary<string, char>();
+
+            // 추가
+            grades.Add("철수", 'A');
+            grades.Add("영희", 'B');
+            if (grades.TryAdd("영희", 'C'))
+            {
+                Console.WriteLine("영희 점수 C 추가");
+            }
+            else
+            {
+                Console.WriteLine($"영희 점수 이미 있음 : {grades["영희"]}");
+            }
+
+            foreach (var sub in grades)
+            {
+                Console.WriteLine(sub.Key);
+                Console.WriteLine(sub.Value);
+            }
+
+            foreach (var sub in grades.Keys)
+            {
+                Console.WriteLine(sub);
+                Console.WriteLine(grades[sub]);
+            }
+
+            
+            // 삭제
+            grades.Remove("철수");
+           
+            // 검색
+            if (grades.ContainsKey("영희"))
+            {
+                Console.WriteLine("영희 점수 있음");
+            }
+
+            if (grades.TryGetValue("영희", out char grade))
+            {
+                Console.WriteLine(grade);
+            }
 
             // Queue
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1); // queue 제일 뒤에 아이템 추가
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Dequeue(); // queue 제일 앞에 있는 아이템 제거 및 반환
+            Console.WriteLine(queue.Dequeue());
+            queue.Peek(); // 제일 앞에있는 아이템 반환
+            queue.TryPeek(out int peek);
 
             // Stack
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            stack.Push(1); // 가장 마지막에 아이템 추가
+            stack.Pop(); // 가장 마지막에 추가된 아이템 제거 및 반환
+            stack.Peek(); // 가장 마지막에 추가된 아이템 반환
+            stack.TryPeek(out int result);
+
         }
     }
 }
