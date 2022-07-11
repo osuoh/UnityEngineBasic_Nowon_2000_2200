63e9ed87e47a62452f646d64adeed1117b356551
Chhhhong <cheonhong450@gmail.com>
2022-07-11 17:33:37 +0900

    C# delegate
    C# delegate

---
+++ Delegate/IOP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void OPDel(int a, int b);
+namespace Delegate
+{
+    internal interface IOP
+    {
+        public OPDel Del { get; set; }
+        public event OPDel DelEvent;
+    }
+
+
+    public class OP : IOP
+    {
+        public OPDel Del { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event OPDel DelEvent;
+    }
+
+
+}
+
---
+++ Delegate/OPs.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class OPs
+    {
+        public enum OP
+        {
+            SUM,
+            SUB,
+            MUL,
+            DIV,
+            MOD
+        }
+
+        public static void Test()
+        {
+            Program.opDelegate += Sum;
+            Program.opDelegate(3, 5);
+        }
+
+        public static bool RefreshOP(OP op, ref Program.MyDelegate opDel)
+        {
+            switch (op)
+            {
+                case OP.SUM:
+                    opDel = Sum;
+                    break;
+                case OP.SUB:
+                    opDel = Sub;
+                    break;
+                case OP.MUL:
+                    opDel = Mul;
+                    break;
+                case OP.DIV:
+                    opDel = Div;
+                    break;
+                case OP.MOD:
+                    opDel = Mod;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool AddOP(OP op, ref Program.MyDelegate opDel)
+        {
+            switch (op)
+            {
+                case OP.SUM:
+                    opDel += Sum;
+                    break;
+                case OP.SUB:
+                    opDel += Sub;
+                    break;
+                case OP.MUL:
+                    opDel += Mul;
+                    break;
+                case OP.DIV:
+                    opDel += Div;
+                    break;
+                case OP.MOD:
+                    opDel += Mod;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool SubOP(OP op, ref Program.MyDelegate opDel)
+        {
+            switch (op)
+            {
+                case OP.SUM:
+                    opDel -= Sum;
+                    break;
+                case OP.SUB:
+                    opDel -= Sub;
+                    break;
+                case OP.MUL:
+                    opDel -= Mul;
+                    break;
+                case OP.DIV:
+                    opDel -= Div;
+                    break;
+                case OP.MOD:
+                    opDel -= Mod;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        static int Sum(int a, int b)
+        {
+            Console.WriteLine($"OP : Sum(), result : {a + b}");
+            return a + b;
+        }
+
+        static int Sub(int a, int b)
+        {
+            Console.WriteLine($"OP : Sub(), result : {a - b}");
+            return a - b;
+        }
+        static int Mul(int a, int b)
+        {
+            Console.WriteLine($"OP : Mul(), result : {a * b}");
+            return a * b;
+        }
+        static int Div(int a, int b) => a / b;
+
+        static int Mod(int a, int b) => a % b;
+    }
+}
