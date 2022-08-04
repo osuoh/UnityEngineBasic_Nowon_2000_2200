47a604d8999778d37eb80e420a0ca67b9c61d625
Chhhhong <Cheonhong450@gmail.com>
2022-08-04 21:52:37 +0900

    Unity RhythmGame 2
    Unity RhythmGame 2

--- RhythmGame/Assets/02.Scripts/NoteHitter.cs
+++ RhythmGame/Assets/02.Scripts/NoteHitter.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class NoteHitter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnDrawGizmosSelected()
     {
-        
-    }
+        // Bad 판정 범위
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireCube(transform.position,
+                            new Vector3(transform.lossyScale.x / 2,
+                                        Constants.HIT_JUDGE_RANGE_BAD,
+                                        0));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Miss 판정 범위
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position,
+                            new Vector3(transform.lossyScale.x / 2,
+                                        Constants.HIT_JUDGE_RANGE_MISS,
+                                        0));
+
+        // Good 판정 범위
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position,
+                            new Vector3(transform.lossyScale.x / 2,
+                                        Constants.HIT_JUDGE_RANGE_GOOD,
+                                        0));
+
+        // Great 판정 범위
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position,
+                            new Vector3(transform.lossyScale.x / 2,
+                                        Constants.HIT_JUDGE_RANGE_GREAT,
+                                        0));
+
+        // Perfect 판정 범위
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position,
+                            new Vector3(transform.lossyScale.x / 2,
+                                        Constants.HIT_JUDGE_RANGE_PERFECT,
+                                        0));
     }
 }
--- RhythmGame/Assets/02.Scripts/NotesManager.cs
+++ RhythmGame/Assets/02.Scripts/NotesManager.cs
@@ -1,18 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class NotesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static float noteSpeedScale = 3f;
+    private Dictionary<KeyCode, NoteSpawner> _spawners = new Dictionary<KeyCode, NoteSpawner>();
+    private Queue<NoteData> _noteDataQueue = new Queue<NoteData>();
+
+    [SerializeField] private Transform _spawnersPoint;
+    [SerializeField] private Transform _noteHittersPoint;
+
+    public float noteFallingDistance
     {
-        
+        get => _spawnersPoint.position.y - _noteHittersPoint.position.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    public float noteFallingTime
     {
-        
+        get => noteFallingDistance / noteSpeedScale;
     }
+
+
+    [SerializeField] private VideoPlayer _videoPlayer;
+
+    public void StartSpawn()
+    {
+        if (_noteDataQueue.Count > 0)
+        {
+            _videoPlayer.clip = SongSelector.instance.clip;
+            _videoPlayer.Play();
+            StartCoroutine(E_Spawning());
+        }
+            
+    }
+
+    IEnumerator E_Spawning()
+    {
+        float startTimeMart = Time.time;
+        while (_noteDataQueue.Count > 0)
+        {
+            for (int i = 0; i < _noteDataQueue.Count; i++)
+            {
+                if (_noteDataQueue.Peek().time < (Time.time - startTimeMart) / noteSpeedScale)
+                {
+                    NoteData noteData = _noteDataQueue.Dequeue();
+
+                    _spawners[noteData.keyCode].SpawnNote().speed *= noteData.speedScale;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            yield return null;
+        }
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(E_Init());
+    }
+
+    IEnumerator E_Init()
+    {
+        NoteSpawner[] spawners = GameObject.Find("NoteSpawners").GetComponentsInChildren<NoteSpawner>();
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            _spawners.Add(spawners[i].keyCode, spawners[i]);
+        }
+
+        yield return new WaitUntil(() => SongSelector.instance != null &&
+                                         SongSelector.instance.isDataLoaded);
+        List<NoteData> notesData = SongSelector.instance.songData.notes;
+        for (int i = 0; i < notesData.Count; i++)
+        {
+            float tmpSpeedScale = 0;
+            if (notesData[i].speedScale > 0)
+                tmpSpeedScale = notesData[i].speedScale;
+            else
+                tmpSpeedScale = 1;
+
+            float timeScaled = notesData[i].time * tmpSpeedScale;
+            notesData[i].time = timeScaled;
+        }
+
+        notesData.Sort((x, y) => x.time.CompareTo(y.time));
+        for (int i = 0; i < notesData.Count; i++)
+        {
+            _noteDataQueue.Enqueue(notesData[i]);
+        }
+    }
+
+   
 }
